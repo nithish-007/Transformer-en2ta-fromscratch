22ef841c0810679da609c41ec8ab5ecdf5ff6104
nithish-007 <itsmedecoder07@gmail.com>
2025-07-21 22:54:35 +0530

    modified multihead attention

--- model.py
+++ model.py
@@ -24,24 +24,32 @@ class InputEnbeddings(nn.Module):
 
 class PositionalEncoding(nn.Module):
     def __init__(self, d_model:int, seq_len:int, dropout:float) -> None:
+        '''
+        pe[:, 0::2] = sin(pos / 10000**(2i/d_model))
+        pe[:, 1::2] = cos(pos / 10000**(2i/d_model))
+        '''
         super().__init__()
         self.d_model = d_model
         self.seq_len = seq_len
         self.dropout = dropout
 
-        # Create a matrix of shape (seq_len, d_model)
+        # Create a pos_enc matrix of shape (seq_len, d_model)
         pe = torch.zeros(seq_len, d_model)
-        # Create a vector of shape (seq_len, 1)
+        # position vector of shape (seq_len, 1)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1) # (seq_len, 1)
-        # Create a vector of shape (d_model)
+
+        # div term in the equation of shape (d_model, )
+        # using log-term instead of direct formula to get a numerical stability (avoid overflow and underflow)
         div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)) #(d_model / 2)
+        
         # apply sine to even indices
-        pe[:, 0::2] = torch.sin(position * div_term) # sin(position / 10000**(2i / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term) 
         # apply cosine to off indices 
-        pe[:, 1::2] = torch.cos(position * div_term) # cos(position / 10000**(2i / d_model))
+        pe[:, 1::2] = torch.cos(position * div_term)
+
         # Add a batch dimension to the positional encoding 
         pe = pe.unsqueeze(0) # (1, seq_len, d_model)
-        # Register the positional encoding as a buffer
+        # Register the positional encoding as a buffer (to avoid it as a learnablr parameter)
         self.register_buffer("pe", pe)
 
     def forward(self, x):
@@ -59,5 +67,19 @@ class MultiHeadAttention(nn.Module):
 
         assert self.d_model % self.h == 0 # d_model is divisible by h
 
-       
-    
+        self.d_k = self.d_model // self.h
+        self.w_q = nn.Linear(self.d_model, self.d_model, bias=False)
+        self.w_k = nn.Linear(self.d_model, self.d_model, bias=False)
+        self.w_v = nn.Linear(self.d_model, self.d_model, bias=False)
+        self.w_o = nn.Linear(self.d_model, self.d_model, bias=False)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def scaled_dot_product(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # 
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores = torch.masked_fill(attention_scores, mask==0, )
+        attention_scores = torch.softmax(attention_scores, dim=1)
