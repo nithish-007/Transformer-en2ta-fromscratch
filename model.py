5d9f6498f9424bf3f41c8377cd6be7bc478d744c
nithish-007 <itsmedecoder07@gmail.com>
2025-07-22 13:07:03 +0530

    Multihead attention code completed and tested

--- model.py
+++ model.py
@@ -83,3 +83,49 @@ class MultiHeadAttention(nn.Module):
         if mask is not None:
             attention_scores = torch.masked_fill(attention_scores, mask==0, )
         attention_scores = torch.softmax(attention_scores, dim=1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+    def forward(self, q, k, v, mask):
+        # replicating inout embeddings into query, key, value using linear transformation
+        query = self.w_q(q) # input(h_i's)(batch, seq_len, d_model) --> query(batch, seq_len, d_model)
+        key = self.w_k(k) # input(h_i's)(batch, seq_len, d_model) --> key(batch, seq_len, d_model)
+        value = self.w_v(v) # input(h_i's)(batch, seq_len, d_model) --> key(batch, seq_len, d_model)
+
+        # split the query, key, value into multiple heads 
+        # (batch, seq_len, d_model) --> (batch, seq_len, h, d_k) --> (batch, h, seq_len, d_model)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1,2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2)
+
+        # Calucate the attention
+        x, self.attention_scores = self.scaled_dot_product(query, key, value, mask, self.dropout)
+
+        # concatanate the heads 
+        # (batch, h, seq_len, d_k) --> (batch, seq_len, h, d_k) --> (batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h*self.d_k)
+
+        # Matrix Transformation (W_o @ x)
+        # (batch, seq_len, d_model) --> (batch, seq_len, d_model)
+        return self.w_o(x)
+
+# # --------------------------------
+# # LayerNormalization
+# # --------------------------------
+
+# class LayerNormalization(nn.Module):
+#     def __init__(self, features: int, eps:float = 10**-6) -> None:
+#         self.eps = eps
+#         self.alpha = nn.Parameter(torch.ones(features))
+#         self.bias = nn.Parameter(torch.zeros)
+
+# # ---------------------
+# # ResidualConnection
+# # ---------------------
+
+# class ResidualConnection(nn.Module):
+#     def __init__(self, features: int, dropout: float) -> None:
+#         super().__init__()
+#         self.dropout = dropout
+#         self.norm = LayerNormalization
