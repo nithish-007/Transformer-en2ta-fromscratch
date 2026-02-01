4f7ef2926e30140b4d27bf77a8f08c65fa8b8e3a
nithish-007 <itsmedecoder07@gmail.com>
2026-02-01 21:28:26 +0530

    Transformer model - Assembled all components into one

--- model.py
+++ model.py
@@ -10,10 +10,24 @@ from decoder import Decoder, DecoderBlock, ProjectionLayer
 # Transformer 
 # -------------------------------
 class Transformer(nn.Module):
+    ''' 
+    Transformer model consisting of encoder and decoder along with embedding, positional encoding, and projection layers.
+    '''
     def __init__(self, encoder: Encoder, decoder: Decoder, 
                  src_embed: EmbeddingLayer, tgt_embed: EmbeddingLayer, 
                  src_pos: SinusoidalPositionalEncoding, tgt_pos: SinusoidalPositionalEncoding, 
                  projection_layer: ProjectionLayer) -> None:
+        
+        '''
+        Args:
+            encoder (Encoder): Encoder network
+            decoder (Decoder): Decoder network
+            src_embed (EmbeddingLayer): Source embedding layer
+            tgt_embed (EmbeddingLayer): Target embedding layer
+            src_pos (SinusoidalPositionalEncoding): Source positional encoding layer
+            tgt_pos (SinusoidalPositionalEncoding): Target positional encoding layer
+            projection_layer (ProjectionLayer): Projection layer to map decoder output to vocabulary size
+        '''
         
         super().__init__()
         self.encoder = encoder
@@ -25,20 +39,47 @@ class Transformer(nn.Module):
         self.projection_layer = projection_layer
 
     def encode(self, src, src_mask):
-        # (batch, seq_len, d_model)
+        '''
+        Args:
+            src (torch.Tensor): Source input tensor of shape (batch, seq_len)
+            src_mask (torch.Tensor): Source mask for attention mechanism
+        
+        Returns:
+            x (torch.Tensor): Encoded output tensor of shape (batch, seq_len, d_model)
+        '''
+        # (batch, seq_len) --> (batch, seq_len, d_model)
         src = self.src_embed(src)
         src = self.src_pos(src)
+
         return self.encoder(src, src_mask)
     
     def decode(self, encoder_output: torch.Tensor, src_mask: torch.Tensor, 
                tgt: torch.Tensor, tgt_mask: torch.Tensor):
-        # (batch, seq_len, d_model)
+        
+        ''' 
+        Args:
+            encoder_output (torch.Tensor): Encoder output tensor of shape (batch, seq_len, d_model)
+            src_mask (torch.Tensor): Source mask for attention mechanism
+            tgt (torch.Tensor): Target input tensor of shape (batch, seq_len)
+            tgt_mask (torch.Tensor): Target mask for attention mechanism
+        
+        Returns:
+            x (torch.Tensor): Decoded output tensor of shape (batch, seq_len, d_model)
+        '''
+        # (batch, seq_len) --> (batch, seq_len, d_model)
         tgt = self.tgt_embed(tgt)
         tgt = self.tgt_pos(tgt)
+
         return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
     
     def project(self, x):
-        # (batch, seq_len, vocab_size)
+        ''' 
+        Args:
+            x (torch.Tensor): Decoder output tensor of shape (batch, seq_len, d_model)
+        Returns:
+            x (torch.Tensor): Logits tensor of shape (batch, seq_len, tgt_vocab_size)
+        '''
+        # (batch, seq_len, d_model) --> (batch, seq_len, tgt_vocab_size)
         return self.projection_layer(x)
 
 # -------------------------------
@@ -47,7 +88,21 @@ class Transformer(nn.Module):
 
 def build_transformer(src_vocab_size: int, tgt_vocab_size: int, src_seq_len: int, tgt_seq_len: int,
                        d_model: int=512, N:int=6, h: int=8, dropout: float=0.1, d_ff: int=2048) ->Transformer:
-    
+    ''' 
+    Args:
+        src_vocab_size (int): Size of the source vocabulary
+        tgt_vocab_size (int): Size of the target vocabulary
+        src_seq_len (int): Maximum sequence length for source
+        tgt_seq_len (int): Maximum sequence length for target
+        d_model (int): Dimension of each embedding vector
+        N (int): Number of encoder and decoder layers
+        h (int): Number of attention heads
+        dropout (float): Dropout rate for regularization
+        d_ff (int): Dimension of the feed-forward network
+        
+    Returns:
+        transformer (Transformer): Assembled Transformer model
+    '''
     # Create the embedding layers
     src_embed = EmbeddingLayer(d_model=d_model, vocab_size=src_vocab_size)
     tgt_embed = EmbeddingLayer(d_model=d_model, vocab_size=tgt_vocab_size)
